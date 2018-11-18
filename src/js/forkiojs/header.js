aa10a26db11b7ddf4942a5bb2aa7f671def66aed
Dmitry Tochilin <ditochilin@gmail.com>
2018-11-18 21:25:55 +0200

    'header'

---
+++ src/js/forkiojs/header.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+	$('.header__burger-icon').click(function(){
+		if($('.menu').hasClass('visible')){
+			$('.menu').removeClass('visible');
+			$(this).removeClass('active');
+		}
+	})
+
+	$('.header__burger-icon').mouseenter(function(){
+		$('.menu').addClass('visible');
+		$(this).addClass('active');
+	})
+
+
+	// $('.menu').hover(function(){
+	// 	$('.menu').toggleClass('visible');
+	// })
+})
